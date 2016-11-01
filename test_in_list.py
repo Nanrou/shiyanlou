10be289522fdcd56cd852af910ea7ce1b409fdf2
Nanrou <kkkcomkkk@qq.com>
2016-11-01 12:23:38 +0800

    nothing

--- test_in_list.py
+++ test_in_list.py
@@ -22,7 +22,8 @@ while i<len(vlist):
     print '--------------------\n'
 '''
 
-vlist = [1,1,3,5,4,4]
+#vlist = [1,1,3,5,4,4]
+vlist=[2,2]
 print vlist,len(vlist)
 t = 1
 score = 0
@@ -43,4 +44,5 @@ while i>0:
     print 'i=%s' % i
     print 'score = %s' % score
     print 'len(vlist) = %s' % len(vlist)
+    print 'vlist = %s' % vlist
     print '--------------------\n'
