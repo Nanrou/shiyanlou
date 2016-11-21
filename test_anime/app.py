55a411579f9f68ea74900a6976b52c13b91d23bb
Nanrou <kkkcomkkk@qq.com>
2016-11-21 23:27:34 +0800

    add app.py

---
+++ test_anime/app.py
@@ -0,0 +1,17 @@
+#coding:utf8
+
+from flask import Flask,render_template,request
+import recommend
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template(index.html)
+@app.route('/search/')
+def search():
+    n = request.args.get('user')    
+    dic = recommend.recommend(n)
+    return render_template('search.html',Data=dic)
+
+if __name__ == '__main__':
+    app.run(port=8000)
